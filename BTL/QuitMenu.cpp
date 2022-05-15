e56cb00b58e59dfb8cadcf22f2ddb5ac7901433a
Duongmanhvp <dinhmanhbh03@gmail.com>
2022-05-15 22:27:12 +0700

    .
    .

---
+++ BTL/QuitMenu.cpp
@@ -0,0 +1,61 @@
+#include "QuitMenu.h"
+
+bool outside(int x, int y, SDL_Rect a)
+{
+     if (x < a.x || y < a.y || x > a.x + a.w || y > a.y + a.h)
+          return false;
+     return true;
+}
+
+bool QuitGame(SDL_Renderer* &gRenderer)
+{
+     SDL_Texture *Texture = loadTexture("images.png", gRenderer);
+     while (true)
+     {
+          SDL_RenderClear(gRenderer);
+          SDL_Rect desrect = {0, 0, 800, 800};
+          SDL_RenderCopy(gRenderer, Texture, NULL, &desrect);
+          SDL_Color gColor = {255, 255, 0};
+          SDL_Rect playRect = renderTextTexture(gRenderer, "VeraMoBd.ttf", "PLAY AGAIN", 30, gColor, 330, 660);
+          SDL_Rect endRect = renderTextTexture(gRenderer, "VeraMoBd.ttf", "EXIT", 30, gColor, 380, 690);
+          SDL_Event e;
+
+          if (SDL_PollEvent(&e) != 0)
+          {
+               if (e.type == SDL_QUIT)
+                    return 0;
+
+               if (e.type == SDL_MOUSEMOTION || e.type == SDL_MOUSEBUTTONDOWN || e.type == SDL_MOUSEBUTTONUP)
+               {
+                    int x, y;
+                    SDL_GetMouseState(&x, &y);
+
+                    if (outside(x, y, playRect) == true)
+                    {
+                         switch (e.type)
+                         {
+                         case SDL_MOUSEMOTION:
+                              break;
+                         case SDL_MOUSEBUTTONDOWN:
+                              return 1;
+                         default:
+                              break;
+                         }
+                    }
+                    if (outside(x, y, endRect) == true)
+                    {
+                         switch (e.type)
+                         {
+                         case SDL_MOUSEMOTION:
+                              break;
+                         case SDL_MOUSEBUTTONDOWN:
+                              return 0;
+                         default:
+                              break;
+                         }
+                    }
+               }
+          }
+          SDL_RenderPresent(gRenderer);
+     }
+}
--- BTL/main.cpp
+++ BTL/main.cpp
@@ -3,79 +3,89 @@
 #include "SnakeMove.h"
 #include "Target.h"
 #include "Menu.h"
+#include "QuitMenu.h"
 
 int main(int argc, char *argv[])
 {
-     // The window we'll be rendering to
-     SDL_Window *gWindow = NULL;
+    // The window we'll be rendering to
+    SDL_Window *gWindow = NULL;
 
-     // The window renderer
-     SDL_Renderer *gRenderer = NULL;
+    // The window renderer
+    SDL_Renderer *gRenderer = NULL;
 
-     // The Texture
-     SDL_Texture *gTexture = NULL;
+    // The Texture
+    SDL_Texture *gTexture = NULL;
 
-     // Crete seed random
-     srand(time(NULL));
+    // Crete seed random
+    srand(time(NULL));
 
-     init(gWindow, gRenderer);
+    init(gWindow, gRenderer);
 
-     if (MenuGame(gRenderer) == true)
-     {
-          gTexture = loadTexture("image.png", gRenderer);
+    if (MenuGame(gRenderer) == true)
+    {
+        bool restart = true;
 
-          SnakeClass snake;
-          Target target;
+        while(restart)
+        {
+            SnakeClass snake;
+            Target target;
 
-          bool quit = false;
-          long long loop = 0;
+            bool quit = false;
+            long long loop = 0;
+            gTexture = loadTexture("snake.jpeg", gRenderer);
 
-          while (!quit)
-          {
-               loop++;
-               SDL_Event e;
+            while (!quit)
+            {
+                loop++;
+                SDL_Event e;
 
-               for (int i = 0; i < SnakeClass().snakeBodyLength; i++)
-               {
+                for (int i = 0; i < SnakeClass().snakeBodyLength; i++)
+                {
                     if (SnakeClass().snakeHead.x == SnakeClass().snakeBody[i].x && SnakeClass().snakeHead.y == SnakeClass().snakeBody[i].y)
                     {
-                         quit = true;
-                         break;
+                        quit = true;
+                        break;
                     }
-               }
+                }
 
-               // Handle events on queue
-               while (SDL_PollEvent(&e) != 0)
-               {
+                // Handle events on queue
+                while (SDL_PollEvent(&e) != 0)
+                {
                     // User requests quit
                     if (e.type == SDL_QUIT || snake.gameOver() == true)
                     {
-                         quit = true;
+                        quit = true;
                     }
                     snake.handleMove(e);
-               }
+                }
 
-               if (loop % 5 == 0)
-                    snake.snakeMove(target.point);
+                if (loop % 5 == 0)snake.snakeMove(target.point);
 
-               // Clear screen
-               SDL_SetRenderDrawColor(gRenderer, 0, 191, 255, 0);
-               SDL_RenderClear(gRenderer);
+                // Clear screen
+                SDL_SetRenderDrawColor(gRenderer, 0, 191, 255, 0);
+                SDL_RenderClear(gRenderer);
 
-               // The Image Background
-               SDL_Rect desrect = {0, 0, 800, 800};
-               SDL_RenderCopy(gRenderer, gTexture, NULL, &desrect);
+                // The Image Background
+                SDL_Rect desrect = {0, 0, 800, 800};
+                SDL_RenderCopy(gRenderer, gTexture, NULL, &desrect);
 
-               snake.renderSnake(gRenderer);
-               target.renderTarget(gRenderer);
+                snake.renderSnake(gRenderer);
+                target.renderTarget(gRenderer);
 
-               // The Scores
-               render_scores(gRenderer, snake.snakeBodyLength);
+                // The Scores
+                render_scores(gRenderer, snake.snakeBodyLength);
 
-               SDL_RenderPresent(gRenderer);
-          }
-     }
+                SDL_RenderPresent(gRenderer);
 
-     QuitSDL(gWindow, gRenderer);
-     return 0;
+                if(quit == true)
+                {
+                    restart = QuitGame(gRenderer);
+                    //if(restart == 1)quit = false;
+                }
+            }
+        }
+    }
+
+    QuitSDL(gWindow, gRenderer);
+    return 0;
 }
